cb0af98ac2828820c4d173f89060a24aefdbcf73
283612294@qq.com <283612294@qq.com>
2018-04-11 18:02:31 +0800

    s

---
+++ bin/demo.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+// var axios = require('../');
+var program  = require('commander');
+program.version('v' + require('../package.json').version)  
+       .description('Manipulate asar archive files')  
+  
+program.command('pack <dir> <output>')  
+       .alias('p')  
+       .description('create asar archive')  
+       .action(function (__dirpath, output) {  
+         asar.geAsar(__dirpath,output);  
+         console.log(output+"文件成功生成");  
+       })  
+program.parse(process.argv)  
+  
+if (program.args.length === 0) {  
+  program.help()  
+}  
